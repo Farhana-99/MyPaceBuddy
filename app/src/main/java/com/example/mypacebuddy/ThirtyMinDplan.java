c25d9b540712a8aeba9867553f182b038cc40daf
Farhana Sharmin <59693653+Farhana-99@users.noreply.github.com>
2020-09-12 08:18:54 +0600

    third commit

--- app/src/main/java/com/example/mypacebuddy/ThirtyMinDplan.java
+++ app/src/main/java/com/example/mypacebuddy/ThirtyMinDplan.java
@@ -6,6 +6,7 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ThirtyMinDplan extends AppCompatActivity {
 
